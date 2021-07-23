170683f2cb2bdf8a73b9821b001f1fb015725640
William <williamjday11@gmail.com>
2021-07-23 11:44:52 +0000

    We are now officially one liners

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,3 @@
+def fizzbuzz(n)
+   n % 15 == 0 ? 'fizzbuzz' : n % 5 == 0 ? 'buzz' : n % 3 == 0 ? 'fizz' : n
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,23 +1,18 @@
 require 'fizzbuzz'
-
 describe 'fizzbuzz' do 
   it 'returns "fizz" when passed 3'  do
-    expect (fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(3)).to eq 'fizz'
   end
 
-describe 'fizzbuzz' do 
   it 'returns "buzz" when passed 5'  do
-    expect (fizzbuzz(5)).to eq 'buzz'
+    expect(fizzbuzz(5)).to eq 'buzz'
   end
-
-describe 'fizzbuzz' do 
+  
   it 'returns "fizzbuzz" when passed 15'  do
-    expect (fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
-
-describe 'fizzbuzz' do 
+  
   it 'returns "4" when passed 4'  do
-    expect (fizzbuzz(4)).to eq '4'
+    expect(fizzbuzz(4)).to eq 4
   end
-
 end  
